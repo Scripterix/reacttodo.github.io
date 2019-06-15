c3b9fdeb4af36c78816cb88cd4866b0b0efe34fc
Scripterix <piotr.adamkowski@gmail.com>
2019-06-15 15:01:26 +0200

    Initial commit React Todo

---
+++ precache-manifest.123a468f7aca452512fe86864e3db518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7d4f5e2c0966b33c3950d9511b0a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1ec7a1de458a9ef2b4",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "5acfd800d23e924fe964",
+    "url": "/static/js/2.7fa37ec6.chunk.js"
+  },
+  {
+    "revision": "bd1ec7a1de458a9ef2b4",
+    "url": "/static/js/main.cc02d5af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
